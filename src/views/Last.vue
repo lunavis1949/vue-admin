158b632a5d7125e6459dc8c0a6790b14d4e1f3af
lunavis <lunavis1949@outlook.com>
2020-09-12 20:41:53 +0800

    构建项目、配置全局模式、路由、elementui

---
+++ src/views/Last.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>哒哒哒哒哒</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "last"
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    登录页
+    <el-button type="primary">buttonCont</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #8cc8ff;
+}
+</style>
